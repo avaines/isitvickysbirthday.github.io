90f79909936f56c6917e05439482ea2dd2da6361
Aiden Vaines <aiden@vaines.org>
2023-10-30 08:34:08 +0000

    adding fireworks

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 // Get the current date
 var currentDate = new Date();
 
+const container = document.querySelector('.fireworks')
+const fireworks = new Fireworks.default(container)
+
 // JS Date is 0 indexed
 if (currentDate.getMonth() === 10 && currentDate.getDate() === 17) {
   document.getElementById("status").textContent = "Yes! Happy Birthday Vicky 🎉";
   document.getElementById("image").src = "images/candles_lit.png";
+  fireworks.start()
 } else {
   document.getElementById("status").textContent = "No 🙁";
   document.getElementById("image").src = "images/candles_unlit.png";
